29cb129b53052ca7c4cc1a2a90a0d1fd700b5a5d
Jiaqian Xu <mengde.wang@mail.utoronto.ca>
2022-07-23 20:23:29 -0400

    test: changed port number

--- frontend/src/_services/socket.service.js
+++ frontend/src/_services/socket.service.js
@@ -1,7 +1,7 @@
 
 import { io } from 'socket.io-client'
 
-export const socket = io('http://localhost:8080/', {
+export const socket = io('https://api.pacer.codes/', {
   transports: ['websocket'],
 });
 
