c2cbfe8b1f378c1928725f483dd7b9329555dbcb
Jiaqian Xu <mengde.wang@mail.utoronto.ca>
2022-07-23 21:08:34 -0400

    debug: backend connection issues

--- frontend/src/_services/socket.service.js
+++ frontend/src/_services/socket.service.js
@@ -1,7 +1,7 @@
 
 import { io } from 'socket.io-client'
 
-export const socket = io('http://localhost', {
+export const socket = io('http://pacer.codes', {
   transports: ['websocket'],
   extraHeaders: {
     "Host": "api.pacer.codes"
